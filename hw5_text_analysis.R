efa4d31ac8eadaca353ba3ec534e66c8960d8785
aidanohara1 <aohara1@bu.edu>
2023-11-17 14:55:35 -0500

    init

---
+++ hw5_text_analysis.R
@@ -0,0 +1,25 @@
+### YOUR NAME HERE ###
+
+### MA 615 Homework 5, text analysis, 2023
+
+### IMPORTANT, DO NOT USE harrypotter library from CRAN
+#devtools::install_github("bradleyboehmke/harrypotter")
+
+# allowed libraries
+library(dplyr)
+library(tidyr)
+library(tidytext)
+library(widyr)
+library(stringr)
+library(harrypotter)
+
+data("chamber_of_secrets")
+
+problem_1a <- 
+problem_1b <-
+
+problem_2a <-
+problem_2b <-
+problem_2c <-
+problem_2d <-
+problem_2e <-
